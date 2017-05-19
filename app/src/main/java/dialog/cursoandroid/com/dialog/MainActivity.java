9711fd1e417552ac7cd003101d65d40e5ac1780d
Cesar <erlan_siqueira@hotmail.com>
2017-05-19 16:22:22 -0300

    Initial commit

---
+++ app/src/main/java/dialog/cursoandroid/com/dialog/MainActivity.java
@@ -0,0 +1,62 @@
+package dialog.cursoandroid.com.dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+    private AlertDialog.Builder dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botaoId);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //criar alert dialog
+                dialog = new AlertDialog.Builder(MainActivity.this);
+
+                // configurar titulo
+                dialog.setTitle("Titulo Dialog");
+
+                //configurar mensagem
+                dialog.setMessage("Mensagem");
+
+                //cancelamento de dialog'
+                dialog.setCancelable(false);
+
+                //botao negativo
+                dialog.setNegativeButton("Nao", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Pressionado botao nao", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Pressionado botao sim", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.create();
+                dialog.show();
+
+            }
+        });
+
+    }
+}
